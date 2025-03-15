e710ad05878310b2f6581925fd32a56f1d8de2d8
Neeraj Kumar <neerajkumar18971@gmail.com>
2025-03-15 23:30:51 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
-import os
-from pathlib import Path
 import argparse
-import zlib
+import os
 import sys
+import zlib
+import hashlib
+from pathlib import Path
+
 
 def init(_):
     os.mkdir(".git")
@@ -28,6 +30,30 @@ def cat_file(args):
     sys.stdout.write(content.decode())
 
 
+def hash_object(args):
+    write_enabled = args.w
+    filepath = Path(args.filepath)
+
+    if not filepath.is_file():
+        print("Object not found.")
+        return
+    with open(filepath, "rb") as f:
+        contents = f.read()
+    size = len(contents)
+    header = f"blob {size}"
+    blob = header.encode() + b"\0" + contents
+    sha_hash = hashlib.sha1(blob).hexdigest()
+    print(sha_hash)
+    if write_enabled:
+        compressed_blob = zlib.compress(blob)
+        sub_dir = sha_hash[:2]
+        obj_name = sha_hash[2:]
+        if not (Path(".git/objects")/sub_dir).is_dir():
+            Path.mkdir(Path(".git/objects")/sub_dir)
+        with open(Path(".git/objects")/sub_dir/obj_name, "wb") as f:
+            f.write(compressed_blob)
+
+
 def main():
     parser = argparse.ArgumentParser()
     subparser = parser.add_subparsers()
@@ -39,6 +65,11 @@ def main():
     cat_file_parser.add_argument("blob_sha")
     cat_file_parser.add_argument("-p", action="store_true")
     cat_file_parser.set_defaults(func=cat_file)
+
+    cat_file_parser = subparser.add_parser("hash-object")
+    cat_file_parser.add_argument("filepath")
+    cat_file_parser.add_argument("-w", action="store_true")
+    cat_file_parser.set_defaults(func=hash_object)
 
     args = parser.parse_args()
     if args.func:
